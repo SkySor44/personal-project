a9e09ec1a84c63700f5f3f1b772109fc55ce43c1
Skyler Sorensen <skylersorensen@Skylers-MacBook-Air.local>
2018-04-23 14:09:28 -0600

    auth0-works

---
+++ server/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const massive = require('massive');
+const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+
+
+const app =  express();
+
+const {
+    SERVER_PORT,
+    CONNECTION_STRING,
+    SESSION_SECRET,
+    DOMAIN,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    CALLBACK_URL
+} = process.env;
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+})
+
+app.use(bodyParser.json());
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Auth0Strategy({
+    domain: DOMAIN,
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: CALLBACK_URL,
+    scope: "openid email profile"
+}, function(accessToken, refreshToken, extraParams, profile, done){
+    done(null, profile)
+}));
+
+app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000',
+    failureRedirect: 'http://localhost:3006/auth'
+}));
+
+passport.serializeUser( (user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser( (user, done) => {
+    done(null, user);
+});
+
+
+
+
+
+
+app.listen(SERVER_PORT, () => console.log(`I'm listening on port ${SERVER_PORT}`));
