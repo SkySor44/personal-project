da43744cfc7f9db1ad1037d7c786ddcfdac4fb3d
Skyler Sorensen <skylersorensen@Skylers-MacBook-Air.local>
2018-05-10 10:57:04 -0600

    createuser-styling

--- src/components/CreateUser/CreateUser.js
+++ src/components/CreateUser/CreateUser.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {finishCreation, getUser2, finishEmployee} from '../../ducks/reducer';
 import {Link} from 'react-router-dom';
+import './CreateUser.css';
 
 class CreateUser extends Component {
     constructor(props) {
@@ -76,7 +77,7 @@ class CreateUser extends Component {
     render() { 
       var rendering =  
       
-      this.state.role === 'employee' ? <div>
+      this.state.role === 'employee' ? <div className = 'createuser-contain'>
             <label>Company: </label>
             <input type = '' value = {this.state.company} onChange = {(e) => this.updateCompany(e.target.value)}/>
             <label>Supervisor Code (given to you by your employer): </label>
@@ -85,28 +86,31 @@ class CreateUser extends Component {
         </div> : 
 
         this.state.role === 'admin' ?
-        <div> 
+        <div className = 'createuser-contain'> 
             <label>Company Name: </label>
             <input type = 'text' value = {this.state.company} onChange = {(e) => this.updateCompany(e.target.value)}/>
             <Link to = {`/home/${this.state.role}`}><button onClick = {() => this.finishCreationAdmin()}>SUBMIT</button></Link>
         </div> : 
         this.state.role === 'client' ?
-        <div>
+        <div className = 'createuser-contain'>
             <label>Home Code (given to you by your contractor): </label>
             <input type = 'text' value = {this.state.supervisor} onChange = {(e) => this.updateSupervisor(e.target.value)}/>
-            <Link to = {`/home/${this.state.role}`}><button onClick = {() => this.finishCreationEmp()}>SUBMIT</button></Link>
+            <Link to = {`/home/${this.state.role}`}><button className = 'two-btns' onClick = {() => this.finishCreationEmp()}>SUBMIT</button></Link>
         </div> :
         
         this.state.role === '' ? 
-        <div>
+        <div className = 'createuser-contain'>
             <h1>Almost Done...</h1>
-                <label>Role: </label>
-                <button onClick = {() => this.updateAdmin()}>Admin</button>
-                <button onClick = {() => this.updateEmployee()}>Employee</button>
-                <button onClick = {() => this.updateClient()}>Client</button>
+                <label>Select Role: </label>
+                <div className = 'createuser-btns'>
+                    <button className = 'two-btns' onClick = {() => this.updateAdmin()}>Admin</button>
+                    <button className = 'two-btns' onClick = {() => this.updateEmployee()}>Employee</button>
+                    <button className = 'two-btns' onClick = {() => this.updateClient()}>Client</button>
+                </div>
+                
         </div> : null
         return ( 
-            <div>
+            <div className = 'createuser-control'>
                {rendering}
             </div>
          )
