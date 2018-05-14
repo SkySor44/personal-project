a7338782362c599da12f8009e9f2f3b9e165f7d5
Skyler Sorensen <skylersorensen@Skylers-MacBook-Air.local>
2018-05-14 10:40:24 -0600

    fixed-message-order-component-did-mount

--- db/get_messages.sql
+++ db/get_messages.sql
@@ -1,3 +1,4 @@
 select message, messages.user_id, project_id, time_stamp, displayname, company, role, supervisor_id from messages
 join users on users.id = messages.user_id
 where messages.project_id = $1 and messages.type = 'project'
+order by messages.id asc
