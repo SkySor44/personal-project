0296450668b6a2ef170ae17af61e679e4adddee6
Skyler Sorensen <skylersorensen@Skylers-MacBook-Air.local>
2018-05-14 12:08:27 -0600

    fixed-client-messages-ordering

--- db/get_client_messages.sql
+++ db/get_client_messages.sql
@@ -1,3 +1,4 @@
 select message, messages.user_id, project_id, time_stamp, displayname, company, role, supervisor_id, type from messages
 join users on users.id = messages.user_id
 where messages.project_id = $1 and messages.type = 'client'
+order by messages.id asc
